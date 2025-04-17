25bb765e2c4c48c52f194f310a53cac75b954324
yeeting0206 <mak960048@gmail.com>
2025-04-18 02:19:10 +0800

    4/18

--- noticeBoard/src/api/announcementApi.js
+++ noticeBoard/src/api/announcementApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8085/api';
+const BASE_URL = 'http://192.168.1.100:8080/api';
 
 export const getAnnouncement = (id) =>
   axios.get(`${BASE_URL}/announcements/${id}`);

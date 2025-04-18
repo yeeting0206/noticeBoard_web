92761c0ed696e3f25d9532cd6ed12e36ae85235d
yiting.lin <mak960048@gmail.com>
2025-04-18 16:07:24 +0800

    修改公告內容輸入框

--- noticeBoard/src/pages/addEdit.jsx
+++ noticeBoard/src/pages/addEdit.jsx
@@ -7,6 +7,8 @@ import {
 } from "../api/announcementApi";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
+import ReactQuill from "react-quill"; 
+import 'react-quill/dist/quill.snow.css'
 
 function AddEdit() {
   const [searchParams] = useSearchParams();
@@ -72,6 +74,11 @@ function AddEdit() {
     }
   };
 
+  const handleContentChange = (value) => {
+    setForm((prev) => ({ ...prev, content: value }));
+  };
+  
+
   return (
     <div className="container my-5">
       <h3 className="mb-4">{id ? "編輯公告" : "新增公告"}</h3>
@@ -124,20 +131,20 @@ function AddEdit() {
             <span>Administrator</span>
           </div>
         </div>
+
         <div className="mb-3 row">
           <label htmlFor="content" className="col-sm-2 col-form-label">
             公告內容:
           </label>
-          <div className="col-sm-10">
-            <textarea
-              id="content"
-              className="form-control"
-              rows="6"
+          <div className="w-full">
+            <ReactQuill
+              theme="snow"
               value={form.content}
-              onChange={handleChange}
+              onChange={handleContentChange}
             />
           </div>
         </div>
+
         <div className="row">
           <div className="offset-sm-2 col-sm-10">
             <button type="submit" className="btn btn-primary">
